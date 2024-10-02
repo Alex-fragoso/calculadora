2469251e54ea20f955197cb89eab4049fa3225b7
Alex-fragoso <alexiromen@gmail.com>
2024-10-02 16:38:33 -0600

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+print("Hola,como estas, soy Alex")
+
+#operaciones matematicas basicas 
+a = 22
+b = 21
+c = 23
+suma = a + b + c
+print(f"la suma de {a}, {b} y {c} es : {suma}")
+resta = c - a - b
+print(f"la resta de {c}, {a} y {b} es : {resta}")
+mult = a * b * c
+print(f" la multimpicacion de {a}, {b} y {c} es : {mult}")
+div =a / (b / c)
+print(f"la divicion de {a}, {b} y {c} es : {div}")
